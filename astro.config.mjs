01291bcb2784b9ab657de47932f016ae5d16fe38
codexplorer24 <mj.aguilargomez@outlook.com>
2026-01-16 15:10:54 +0100

    adaptación a netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+
+// CAMBIO: Importamos Netlify y quitamos Node
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  // 1. Tu dominio
   site: 'https://tuprimerhogar.es',
-
-  // 2. CAMBIO CRUCIAL: Activamos el modo servidor (SSR)
   output: 'server',
 
-  // 3. Integraciones
   integrations: [tailwind(), sitemap(), react()],
 
-  // 4. Dominios permitidos para imágenes (Añadimos la IP numérica)
   image: {
-    domains: ["localhost", "127.0.0.1"],
+    domains: ["localhost", "127.0.0.1", "res.cloudinary.com"], // Añade Cloudinary por si acaso
   },
 
-  // 5. Configuración del servidor Node.js
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // CAMBIO: Usamos el adaptador de Netlify
+  adapter: netlify(),
 });
