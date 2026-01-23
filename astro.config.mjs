0eae4006ce6e9856dfa32042be8f7f936cea9b02
codexplorer24 <mj.aguilargomez@outlook.com>
2026-01-23 10:41:44 +0100

    cambiamos configuración para desplegar en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
-// CAMBIO: Importamos Netlify y quitamos Node
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: 'https://tuprimerhogar.es',
@@ -13,9 +12,9 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), react()],
 
   image: {
-    domains: ["localhost", "127.0.0.1", "res.cloudinary.com"], // Añade Cloudinary por si acaso
+    domains: ["localhost", "127.0.0.1", "res.cloudinary.com"], 
   },
 
-  // Usamos el adaptador de Netlify
-  adapter: netlify(),
+  // Usamos el adaptador de Vercel para desplegar en Vercel el prototipo
+  adapter: vercel(),
 });
